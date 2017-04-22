67ae1b7e7a0dac8d23ba9f5b688cf9e58b7877a9
Kristin Caron <krissycaron@gmail.com>
2017-04-22 12:23:34 -0500

    working on printing the card to the dom

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,31 @@
 // Use AJAX | Promises to load all 3 JSON files
 $(document).ready(function(){
+const outputContainer = $("#output");
+
+
+//////////////////////////////////////////////////////////////
+//////////////// DOM functions are going here////////////////
+
+const writeToDOM = ((myCharacters)=>{
+	let domString = "";
+	for (let m=0; m < myCharacters.length; m++) {
+		domString += `<div class="character row">`;
+		domString += `<div class="col-sm-4">`;
+		domString += `<img src="${myCharacters[m].image}">`;
+		domString += `</div>,</div>`;
+		console.log(myCharacters);
+
+	}
+	outputContainer.append(domString);
+
+
+});
+
+
+console.log("It's getting  to line 24");
+///////////////////////////////////////////////////////////////////
+///// AJAX Calls getting and returning the JSON data files ///////
+///////////////////////////////////////////////////////////////////
 
 const loadCharacters =()=> {
 	return new Promise((resolve, reject)=>{
@@ -25,7 +51,12 @@ const loadTeams=()=> {
 	});
 };
 
+///// End of AJAX calls ///////
+///////////////////////////////
+
+
 let charactersArray = [];
+let teamsArray = [];
 
 
 //characters are what i will be printing and will be what data i want to be sorting. 
@@ -35,9 +66,27 @@ loadCharacters().then((characterA) => {
 		charactersArray.push(characterB);
 
 	});
-
-
 // console.log(charactersArray); getting this to print. :-) 
+	Promise.all([loadGenders(), loadTeams()])
+	.then((result)=>{
+		// console.log(result);
+		result.forEach((xhrResults)=> {
+			xhrResults.forEach((teamsArray)=> {
+				charactersArray.push(teamsArray);
+			});
+		});
+
+		for (let i=0; i<charactersArray.length; i++){
+			for (let k=0; k<teamsArray.length; k++){
+		console.log(charactersArray[i], teamsArray[k]);
+				// if ()
+				// charactersArray[i].matches.push(teamsArray[k]);
+		writeToDOM(charactersArray);
+			} 
+		}
+
+	});
+
 
 });
 
