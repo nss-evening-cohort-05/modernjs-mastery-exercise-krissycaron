6c5d688f438a869a8ce6c27ce52cd70f8b001b14
Kristin Caron <krissycaron@gmail.com>
2017-05-03 22:25:02 -0500

    wrapping promise in function to meet requirements

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,12 +6,17 @@ $(document).ready(function(){
 //////////////////////////////////////////////////////////////
 //////////////// DOM functions are going here////////////////
 
+
+
+
+
+
 const writeToDOM = ((result)=>{
 	let domString = "";
-		// console.log("WTF", result[2]);
+		
 	for (let m=0; m < result[2].length; m++) {
-		// console.log("result", result[2]);
-		domString += `<div class="container row characterCard">`;
+		
+		domString += `<div class="container characterCard">`;
 		domString += `<img src="${result[2][m].image}">`;
 		domString += `<p>${result[2][m].name}</p>`;
 		domString += `<p>${result[2][m].description}</p>`;
@@ -23,7 +28,7 @@ const writeToDOM = ((result)=>{
 });
 
 
-// console.log("It's getting  to line 26");
+
 ///////////////////////////////////////////////////////////////////
 ///// AJAX Calls getting and returning the JSON data files ///////
 ///////////////////////////////////////////////////////////////////
@@ -56,50 +61,30 @@ const loadTeams=()=> {
 ///////////////////////////////
 
 
-let charactersArray = [];
+
 let teamsArray = [];
 let genderArray = [];
 let characters;
 
-
-// characters are what i will be printing and will be what data i want to be sorting. 
-// loadCharacters().then((characterA) => {
-// 	characterA.forEach((characterB) => {
-// 		characterB.matches = [];
-// 		charactersArray.push(characterB);
-
-// 	});
-// console.log(charactersArray); getting this to print. :-) 
-	Promise.all([loadGenders(), loadTeams(), loadCharacters()])
+	const dataGetter = (teamID) => {Promise.all([loadGenders(), loadTeams(), loadCharacters()])
+	console.log(teamID);
 	.then((result)=>{
-		// console.log("genders",result[0]);
-		// console.log("teams",result[1]);
-		// console.log("characters",result[2]);
-		result[0].forEach((gender)=>{
-			// console.log(gender);
-			result[1].forEach((teams) => {
-				// console.log(teams);
-				result[2].forEach((characters)=> {
-					// console.log(characters);
-				}); 
+		let charactersArray = [];
+		result.forEach((chars)=>{
+			chars.forEach((teams) => {
+				if (teamID === teams.team_id){
+					charactersArray.push(teams);
+				}
 			});
 		});
+	
+	writeToDOM(result);
+	});
+
+};
 
 
-		writeToDOM(result);
-		// for (let i=0; i<characters.length; i++){
-		// 	for (let k=0; k<teamsArray.length; k++){
-		// 		charactersArray[i].matches.push(teamsArray[k]);
-		// 	} 
-		// }
-
-
-});
-
-
-
-
-
+console.log("dataGetter", dataGetter);
 
 
 });
